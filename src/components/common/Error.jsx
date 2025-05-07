04e3afc26f7cc06c397e3fb7266afe0d81234468
Songdoeon <ehdjs9583@naver.com>
2025-05-07 13:38:08 +0900

    fix : error.jsx 에러 수정

--- src/components/common/Error.jsx
+++ src/components/common/Error.jsx
@@ -1,7 +1,7 @@
 const Error = ({ error }) => {
   return (
     <div className='flex flex-col items-center justify-center mb-4'>
-      <p className='text-red-500'>{error}</p>
+      <p className='text-red-500'>{error.message}</p>
     </div>
   );
 };
